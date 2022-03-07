fa3b4b19550ba377ffc00c7731f970c91a2e1a71
Jonathan Adams <jonsadams@icloud.com>
2022-03-07 11:19:55 -0500

    First push

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function openNamingConventions() {
+    let x =document.getElementById("openConventions");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
